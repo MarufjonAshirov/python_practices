f1c2cc049aa227da3cc9c97ee8ec2f3de217285e
Marufjon Ashirov <marufjon0825@gmail.com>
2022-07-01 14:09:18 +0500

    added multifunctional calculator

---
+++ com/practice/calculator/calculator.py
@@ -0,0 +1,27 @@
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+def power(n1, n2):
+    return n1 ** n2
+
+
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+    '^': power
+}
