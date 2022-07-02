1515f7b99f8145397249320e87bed10030e5e660
Marufjon Ashirov <marufjon0825@gmail.com>
2022-07-02 23:40:23 +0500

    updated multifunctional calculator

--- com/practice/calculator/calculator.py
+++ com/practice/calculator/calculator.py
@@ -1,3 +1,6 @@
+from calculator_art import logo
+
+
 def add(n1, n2):
     return n1 + n2
 
@@ -18,6 +21,15 @@ def power(n1, n2):
     return n1 ** n2
 
 
+def operation_check(operation):
+    if operation in operations:
+        return operation
+    else:
+        print('invalid input')
+        operation = input('Pick an operation above: ')
+        return operation_check(operation)
+
+
 operations = {
     '+': add,
     '-': subtract,
@@ -25,3 +37,32 @@ operations = {
     '/': divide,
     '^': power
 }
+
+
+def calculator():
+    print(logo)
+    num1 = float(input('Please enter first number: '))
+    total = 0
+    finished = False
+    while not finished:
+        for op in operations:
+            print(op)
+        operation_symbol = input('Pick an operation above: ')
+        check_symbol = operation_check(operation_symbol)
+        num2 = float(input('please enter next number: '))
+        function_calc = operations[check_symbol]
+        answer = function_calc(num1, num2)
+        print(f'{num1} {check_symbol} {num2} = {answer}')
+        exiting = 'tab anything to exit'
+        ask = input(f'type "y" to continue calculating with {answer}, or type "n" to restart program or {exiting}: ')
+        if ask == 'y':
+            num1 = answer
+        elif ask == 'n':
+            finished = True
+            calculator()
+        else:
+            finished = True
+    print('Thanks for using my program\nBye')
+
+
+calculator()
