49927041644786ce708553e25dda1abd6da542c7
Marufjon Ashirov <marufjon0825@gmail.com>
2022-07-16 19:43:04 +0500

    completed blackjack game

--- com/practice/blackjack_game/blackjack.py
+++ com/practice/blackjack_game/blackjack.py
@@ -16,29 +16,54 @@ def calculate_score(cards_amount):
     return sum(cards_amount)
 
 
-print(logo)
-user_cards = []
-computer_cards = []
-is_game_over = False
-for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
-print(f'your cards: {user_cards}, current score: {user_score}')
-print(f'computers first card: {computer_cards[0]}')
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
-else:
-    user_should_deal = input('type "y" to get another card, type "n" to pass')
-    if user_should_deal == 'y':
-        user_cards.append(deal_card())
+def compare(user_score, computer_score):
+    if user_score > 21 and computer_score > 21:
+        return "You went over. You lose 😤"
+
+    if user_score == computer_score:
+        return "Draw 🙃"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack 😱"
+    elif user_score == 0:
+        return "Win with a Blackjack 😎"
+    elif user_score > 21:
+        return "You went over. You lose 😭"
+    elif computer_score > 21:
+        return "Opponent went over. You win 😁"
+    elif user_score > computer_score:
+        return "You win 😃"
     else:
-        is_game_over = True
-    print(user_cards)
-    print(computer_cards)
-    print('this is test to blackjack')
-    print('this is second test')
-    print('this is third test')
-    print('this is forth test')
-    print('this is 5 test')
+        return "You lose 😤"
+
+
+def play_game():
+    print(logo)
+    user_cards = []
+    computer_cards = []
+    is_game_over = False
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f'your cards: {user_cards}, current score: {user_score}')
+        print(f'computers first card: {computer_cards[0]}')
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input('type "y" to get another card, type "n" to pass: ')
+            if user_should_deal == 'y':
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"Your final hand: {user_cards}, your score {user_score}")
+    print(f"Computer;s hand: {computer_cards}, computer's score: {computer_score}")
+    print(compare(user_score, computer_score))
+
